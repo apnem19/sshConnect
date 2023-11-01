eac8dbba4564c87714ee0fe5c55ae7179f92c416
apnem19 <63157221+apnem19@users.noreply.github.com>
2023-11-01 10:36:05 +0300

    Update SSHConnect.php

--- src/SSHConnect.php
+++ src/SSHConnect.php
@@ -2,24 +2,36 @@
 
 namespace apnem19\SSHConnect;
 
-use phpseclib\Crypt\RSA;
-use phpseclib\Net\SCP;
 use phpseclib\Net\SSH2;
+use phpseclib\Net\SFTP;
 
 class SSHConnect
 {
     private mixed $output;
     private mixed $error;
-    private SSH2 $ssh;
-
-    public function __construct(string $hostname, string $username, string $password, int $port = 22, int $timeout)
+    private SSH2|SFTP $ssh;
+	public const TYPE_SFTP = "SFTP";
+	public const TYPE_SSH = "SSH";
+	
+    public function __construct(string $type, string $hostname, string $username, string $password, int $port = 22)
     {
-        $ssh = new SSH2($hostname, $port);
-        if ($ssh->login($username, $password)) {
-             $this->ssh = $ssh;
-        } else {
-            throw new \RuntimeException('Unable to connect to server');
-        }
+		if($type == self::TYPE_SFTP){
+			$sftp = new SFTP($hostname);
+			if ($sftp->login($username, $password)) {
+				 $this->ssh = $sftp;
+			} else {
+				throw new \RuntimeException('Unable to connect to server');
+			}
+		}elseif($type == self::TYPE_SSH){
+			$ssh = new SSH2($hostname, $port);
+			if ($ssh->login($username, $password)) {
+				 $this->ssh = $ssh;
+			} else {
+				throw new \RuntimeException('Unable to connect to server');
+			}
+		}else{
+			throw new \RuntimeException('Error TYPE');
+		}
     }
 
     public function exec($exec): void
@@ -28,29 +40,17 @@ class SSHConnect
         $this->output = $this->ssh->exec($exec);
         $this->error = $this->ssh->getStdError();
     }
+	
+	public function open($command) {
+		return $this->ssh->get($command);
+	}
+	
+	public function write($command, $file) {
+		return $this->ssh->put($command, $file);
+	}
+	
+	public function get($path, $fun) {
+		return @$this->ssh->$fun($path);
+	}
 
-    public function getRawOutput(): string
-    {
-        return $this->output;
-    }
-
-    public function getRawError(): string
-    {
-        return $this->error;
-    }
-
-    public function getOutput(): string
-    {
-        return trim($this->getRawOutput());
-    }
-
-    public function getError(): string
-    {
-        return trim($this->getRawError());
-    }
-
-    public function __debugInfo(): array
-    {
-        return ['output' => $this->getRawOutput(), 'error' => $this->getRawError()];
-    }
 }
